f418c3f17eaf68cbf2d2a5a62d714fbeb0756367
Veerapandian V R <146498899+Veerapandian16@users.noreply.github.com>
2025-06-10 19:42:02 +0530

    Create ArmstrongNumber.java

---
+++ Basic Programs/ArmstrongNumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Arm_Strong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int originalNum = num;
+        int result = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num != 0) { //or while(num > 0)
+            int digit = num % 10;
+            result += Math.pow(digit, digits);
+            num /= 10; // or num = num / 10
+        }
+
+        if (result == originalNum) {
+            System.out.println(originalNum + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNum + " is not an Armstrong number.");
+        }
+
+        sc.close();
+    }
+}
