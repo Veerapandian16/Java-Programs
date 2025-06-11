10603102658ab3fa19b9d9ae8fc057ea32a7356b
Veerapandian V R <146498899+Veerapandian16@users.noreply.github.com>
2025-06-11 20:58:59 +0530

    Create SumOf2Integers.java

---
+++ Basic Programs/SumOf2Integers.java
@@ -0,0 +1,15 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class SumOfTwoIntegers {
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = a + b;
+    System.out.println("Sum: " + c);
+        
+    }
+ 
+}
