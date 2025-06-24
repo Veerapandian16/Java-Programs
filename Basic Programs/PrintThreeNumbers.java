0b000713a1bf9be07258d84403a302df8131ede3
Veerapandian V R <146498899+Veerapandian16@users.noreply.github.com>
2025-06-24 23:06:20 +0530

    Create PrintThreeNumbers.java

---
+++ Basic Programs/PrintThreeNumbers.java
@@ -0,0 +1,15 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class PrintThreeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
